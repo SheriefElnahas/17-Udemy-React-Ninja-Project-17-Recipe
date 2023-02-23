4f910c7753cd9d8ce2397b03d899505afebf38ea
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-02-24 01:31:59 +0200

    fetching recipes data from json file

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Recipe({ title, time, description }) {
+function Recipe({ title, description, cookingTime }) {
   return (
     <div className="Recipe">
       <h2 className="Recipe-title">{title}</h2>
-      <p className="Recipe-time">{time} minutes to make</p>
+      <p className="Recipe-time">{cookingTime} minutes to make</p>
       <p className="Recipe-description">{description}</p>
       <button className="Recipe-btn">Cook This</button>
     </div>
